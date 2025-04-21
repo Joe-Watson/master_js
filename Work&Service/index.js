50339675bdcc9b6a923b9f53bd5504b8dd71615f
Suman kumar <joewatson881178@gmail.com>
2025-04-21 13:36:14 +0530

    heavy work working now

--- Work&Service/index.js
+++ Work&Service/index.js
@@ -1,11 +1,11 @@
 const worker = new Worker('./worker.js');
-const heavyWorker=new Worker('./heavyWorker.js');
+const heavyWorker=new Worker('./HeavyWork.js')
 
-// worker.onmessage = function (event) {
-//     console.log('Result from worker:', event.data);
-// }
+worker.onmessage = function (event) {
+    console.log('Result from worker:', event.data);
+}
 
-// worker.postMessage(21);
+worker.postMessage(21);
 
 heavyWorker.onmessage=function(event){
     console.log('Result from heavyWorker:', event.data);
