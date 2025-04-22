d0354d2b90011dea6d574b0a3caf123f093684f8
Suman kumar <joewatson881178@gmail.com>
2025-04-22 14:43:36 +0530

    added Factory Pattern

---
+++ DesignPattner/FactoryPattern/Users.js
@@ -0,0 +1,29 @@
+class AdminUser{
+    constructor(){
+        this.role='admin';
+        this.permissions=['read','write','delete'];
+    }
+}
+
+class GuestUser{
+    constructor(){
+        this.role='guest';
+        this.permissions=['read'];
+    }
+}
+
+class UserFactory{
+    static createUser(type){
+        switch(type){
+            case "admin":
+                return new AdminUser();
+            case "guest":
+                return new GuestUser();
+            default:
+                throw new Error("Invalid user type");
+        }
+    }
+}
+
+const admin=UserFactory.createUser('admin');
+console.log(admin.role,admin.permissions);
